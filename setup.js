ac8a4bd7e527e8241aa449f123af5a9f332923b1
afcajamarcar <afcajamarcar@unal.edu.co>
2018-10-18 05:38:44 -0500

    added static candidates and users

--- setup.js
+++ setup.js
@@ -1,9 +1,44 @@
 let res = [
-	db.createUser({
-		user: 'admin',
-		pwd: 'votaciones2018.',
-		roles: [
-			{role: 'dbOwner', db: 'votaciones'}
-		]
-	})
+  db.createUser({
+    user: 'admin',
+    pwd: 'votaciones2018.',
+    roles: [
+      {role: 'dbOwner', db: 'votaciones'}
+    ]
+  }),
+  db.createCollection('usersCollection'),
+  db.usersCollection.insert([
+    {
+      "name" : "Jonathan Ortiz",
+      "id" : "10145367895",
+      "email" : 'joaortizro@unal.edu.co',
+      "password": "asd321",
+      "hasVoted" : false,
+      "isCandidate" : true,
+      "imagePath" : ""
+    },
+    {
+      "name" : "Anni Piragauta",
+      "id" : "1034546789",
+      "email" : 'annilok@unal.edu.co',
+      "password": "asd123",
+      "hasVoted" : false,
+      "isCandidate" : true,
+      "imagePath" : ""
+    },
+    {
+      "name" : "Blanco",
+      "id" : "0",
+      "email" : 'walter@white.com',
+      "isCandidate" : true
+    },
+    {
+      "name" : "UserTest",
+      "id" : "666",
+      "email" : 'test@unal.edu.co',
+      "password": "12345678",
+      "hasVoted" : false,
+      "isCandidate" : false
+    }
+  ])
 ]
